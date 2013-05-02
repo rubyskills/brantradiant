aeaf42565cea6e4b97035f7996c5028ed0906bd1
Hakki <hakki@hakki-VirtualBox.(none)>
2013-05-02 12:40:50 -0400

    java script fixes

--- js/main.js
+++ js/main.js
@@ -1,16 +1,32 @@
-$(function() {
+var findMarket = function() {
     var market = window.location.pathname.split('/')[2];
     var selectText = '';
-    var carot = "\n<span class=\"caret\"></span>";
+    var selectHref = '';
+
+    if (!market) { return null }
+
     $('#market-dropdown li a').each(function(index, a) {
 	var text = $(a).text();
+	var href = $(a).attr('href');
 	var compareMarket = text.toLowerCase();
 	if (compareMarket.replace(/[^a-zA-Z]+/, '') == market.replace(/[^a-zA-Z]+/, '')) {
 	    selectText = text;
+	    selectHref = href;
 	}
     });
 
-    if (market != undefined) {
-	$('#dLabel').html(selectText + carot)
+    return {name: market, text: selectText, href: selectHref}
+}
+
+var productLink = function(href) {
+    $('#products_link').attr('href', href)
+}
+
+$(function() {
+    var market = findMarket()
+    if (market != null) {
+	var carot = "\n<span class=\"caret\"></span>";
+	$('#dLabel').html(market.text + carot)
+	productLink(market.href)
     }
 });
